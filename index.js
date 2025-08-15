151d2ff0f2d4505a64c4dea53ed926291d10f6f7
Alok <ae1raj21@gmail.com>
2025-08-15 09:54:32 +0530

    Add comments to clarify event listener implementation and chat function execution

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const ChatApp = require("./chatApp.js");
 
 const chat = new ChatApp();
 
+//Implementing event listeners for chat events
 chat.on('join', (user) => {
     console.log(`${user} has joined the chat.`)
 });
@@ -14,6 +15,7 @@ chat.on("leave", (user) => {
     console.log(`${user} has left the chat.`);
 })
 
+//Running the chat functions
 chat.join("Phineas");
 chat.join("Ferb");
 
